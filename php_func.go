c2f0c06fb8290c44786aad27c57db583cc5f8fe3
pz <2912150017@qq.com>
2023-06-03 22:52:11 +0800

    add php func

---
+++ php_func.go
@@ -0,0 +1,15 @@
+package helpers
+
+func Addslashes(str string) string {
+	var tmpRune []rune
+	for _, ch := range str {
+		switch ch {
+		case []rune{'\\'}[0], []rune{'"'}[0], []rune{'\''}[0]:
+			tmpRune = append(tmpRune, []rune{'\\'}[0])
+			tmpRune = append(tmpRune, ch)
+		default:
+			tmpRune = append(tmpRune, ch)
+		}
+	}
+	return string(tmpRune)
+}
