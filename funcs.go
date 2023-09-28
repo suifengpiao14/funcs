318a6f369983aadd5b34c46b84174788ddbdf3b4
zheng peng <pengzheng@huishoubao.com.cn>
2023-09-28 09:41:47 +0800

    add some func

--- funcs.go
+++ funcs.go
@@ -1,9 +1,11 @@
 package funcs
 
 import (
+	"encoding/json"
 	"reflect"
 
 	"github.com/pkg/errors"
+	"github.com/spf13/cast"
 )
 
 func StructToMap(obj interface{}) map[string]interface{} {
@@ -25,4 +27,24 @@ func StructToMap(obj interface{}) map[string]interface{} {
 		resultMap[tag] = fieldValue.Interface()
 	}
 	return resultMap
+}
+
+//Struct2MapString 结构体转json
+//使用场景 resty curl 构造请求参数时常用
+func Struct2MapString(i interface{}) (out map[string]string, err error) {
+
+	var myMap map[string]interface{}
+	data, err := json.Marshal(i)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(data, &myMap)
+	if err != nil {
+		return nil, err
+	}
+	out = make(map[string]string)
+	for k, v := range myMap {
+		out[k] = cast.ToString(v)
+	}
+	return out, nil
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/pkg/errors v0.9.1
+	github.com/spf13/cast v1.5.1
 	github.com/stretchr/testify v1.8.4
 	golang.org/x/text v0.9.0
 )
