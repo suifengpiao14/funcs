e57fcef36b874afde3a9b29ee3c332d6154e4d2e
pz <2912150017@qq.com>
2026-03-10 22:28:07 +0800

    alter CreateNewInstance

--- common.go
+++ common.go
@@ -17,9 +17,8 @@ func IsNil(v interface{}) bool {
 }
 
 // CreateNewInstance 创建T类型的新实例（兼容指针/非指针类型）
-func CreateNewInstance[T any]() T {
-	var zero T
-	rv := reflect.ValueOf(&zero).Elem() // 获取T类型的反射值
+func CreateNewInstance[T any]() (t T, isPtr bool) {
+	rv := reflect.ValueOf(&t).Elem() // 获取T类型的反射值
 
 	// 构建新实例的反射值
 	var newVal reflect.Value
@@ -28,10 +27,12 @@ func CreateNewInstance[T any]() T {
 		elemType := rv.Type().Elem()     // 获取指针指向的元素类型
 		newElem := reflect.New(elemType) // 创建元素的指针（*elemType）
 		newVal = newElem
+		isPtr = true
 	} else {
 		// 情况2：T是非指针类型 → 直接创建新实例
 		newVal = reflect.New(rv.Type()).Elem() // 创建T类型的实例
 	}
 	// 将反射值转换为T类型并返回
-	return newVal.Interface().(T)
+	t = newVal.Interface().(T)
+	return t, isPtr
 }
